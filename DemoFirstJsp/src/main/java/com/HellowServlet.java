632a73795912024a2864d236d1fdfeedec3ea3b1
NajeshPanigrahi <najeshpanigrahi@gmail.com>
2024-03-03 22:11:23 +0530

    my 1st push

---
+++ DemoFirstJsp/src/main/java/com/HellowServlet.java
@@ -0,0 +1,22 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HellowServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		PrintWriter printWriter = response.getWriter();
+		String s = request.getParameter("refJsp");
+		printWriter.println("<h1>Hellow, " + s);
+		printWriter.close();
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
---
+++ DemoFirstJsp/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+	<form method="get" action="HellowServlet">
+		<pre>
+Enter your name: <input type="text" name="refJsp">
+	<input type=submit>  <input type=reset>
+</pre>
+	</form>
+</body>
+</html>
